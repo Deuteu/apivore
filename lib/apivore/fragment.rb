b121b9de4137ab207a2d6f3394e7bedf2bf7a512
Deuteu <Deuteu@users.noreply.github.com>
2018-08-05 14:32:09 +0200

    Apply rubocop to Fragment.

--- lib/apivore/fragment.rb
+++ lib/apivore/fragment.rb
@@ -2,7 +2,7 @@ module Apivore
   # This is a workaround for json-schema's fragment validation which does not allow paths to contain forward slashes
   #  current json-schema attempts to split('/') on a string path to produce an array.
   class Fragment < Array
-    def split(options = nil)
+    def split(_options = nil)
       self
     end
   end
