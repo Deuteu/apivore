cf424767ca36c898f373414235e466e5e2ea6afe
Deuteu <Deuteu@users.noreply.github.com>
2018-08-05 15:23:41 +0200

    Apply rubocop to apivore specs.

--- spec/apivore_spec.rb
+++ spec/apivore_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 describe 'Apivore::Swagger' do
-
   context 'with valid swagger 2.0 input' do
-    let(:doc) { IO.read(File.join(File.dirname(__FILE__), "data", "01_sample2.0.json")) }
+    let(:doc) { IO.read(File.join(File.dirname(__FILE__), 'data', '01_sample2.0.json')) }
     let(:swagger) { Apivore::Swagger.new(JSON.parse(doc)) }
 
     subject { swagger }
@@ -24,21 +23,21 @@ describe 'Apivore::Swagger' do
     end
 
     describe 'each_response' do
-      it "should return the responses" do
+      it 'should return the responses' do
         expect { |b| swagger.each_response(&b) }.to yield_successive_args(
-          ["/services.json", "get", "200", ['#', 'paths', '/services.json', 'get', 'responses', '200', 'schema']],
-          ["/services.json", "post", "204", nil],
-          ["/services/{id}.json", "get", "200", ['#', 'paths', '/services/{id}.json', 'get', 'responses', '200', 'schema']],
-          ["/services/{id}.json", "put", "204", nil],
-          ["/services/{id}.json", "delete", "204", nil],
-          ["/services/{id}.json", "patch", "204", nil]
+          ['/services.json', 'get', '200', ['#', 'paths', '/services.json', 'get', 'responses', '200', 'schema']],
+          ['/services.json', 'post', '204', nil],
+          ['/services/{id}.json', 'get', '200', ['#', 'paths', '/services/{id}.json', 'get', 'responses', '200', 'schema']],
+          ['/services/{id}.json', 'put', '204', nil],
+          ['/services/{id}.json', 'delete', '204', nil],
+          ['/services/{id}.json', 'patch', '204', nil]
         )
       end
     end
   end
 
   context 'with invalid swagger input' do
-    let(:doc) { IO.read(File.join(File.dirname(__FILE__), "data", "invalid_swagger.json")) }
+    let(:doc) { IO.read(File.join(File.dirname(__FILE__), 'data', 'invalid_swagger.json')) }
     let(:swagger) { Apivore::Swagger.new(JSON.parse(doc)) }
 
     subject { swagger }
