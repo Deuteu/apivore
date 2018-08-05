631764e6bd8546581167a856fd8b4c67949631c3
Deuteu <Deuteu@users.noreply.github.com>
2018-08-05 15:20:29 +0200

    Apply rubocop to request specs.

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,42 +1,32 @@
 require 'spec_helper'
 
-describe "Example API", type: :apivore, order: :defined do
-  subject { Apivore::SwaggerChecker.instance_for("/swagger-doc.json") }
+describe 'Example API', type: :apivore, order: :defined do
+  subject { Apivore::SwaggerChecker.instance_for('/swagger-doc.json') }
 
-  context "has valid paths" do
+  context 'has valid paths' do
     it 'allows the same path (with response documented) to be tested twice' do
-      expect(subject).to validate(:get, "/services.json", 200)
-      expect(subject).to validate(:get, "/services.json", 200)
+      expect(subject).to validate(:get, '/services.json', 200)
+      expect(subject).to validate(:get, '/services.json', 200)
     end
 
     it do
-      expect(subject).to validate(
-        :post, "/services.json", 204, {'name' => 'hello world'}
-      )
+      expect(subject).to validate(:post, '/services.json', 204, 'name' => 'hello world')
     end
 
     it do
-      expect(subject).to validate(
-        :get, "/services/{id}.json", 200, {'id' => 1}
-      )
+      expect(subject).to validate(:get, '/services/{id}.json', 200, 'id' => 1)
     end
 
     it do
-      expect(subject).to validate(
-        :put, "/services/{id}.json", 204, {'id' => 1}
-      )
+      expect(subject).to validate(:put, '/services/{id}.json', 204, 'id' => 1)
     end
 
     it do
-      expect(subject).to validate(
-        :delete, "/services/{id}.json", 204, {'id' => 1}
-      )
+      expect(subject).to validate(:delete, '/services/{id}.json', 204, 'id' => 1)
     end
 
     it do
-      expect(subject).to validate(
-        :patch, "/services/{id}.json", 204, {'id' => 1}
-      )
+      expect(subject).to validate(:patch, '/services/{id}.json', 204, 'id' => 1)
     end
   end
 
@@ -48,11 +38,11 @@ describe "Example API", type: :apivore, order: :defined do
     it 'additionally conforms to a custom schema' do
       expect(subject).to conform_to(Apivore::CustomSchemaValidator::WF_SCHEMA)
     end
+
     # it 'has definitions consistent with the master docs' do
     #   expect(subject).to be_consistent_with_swagger_definitions(
     #     "api.westfield.io", 'deal'
     #   )
     # end
   end
-
 end
